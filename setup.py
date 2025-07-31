e6f0cde416ac531cf544cc4056f32ca8876681fe
Tapas Walde <tapaswalde@gmail.com>
2025-07-31 20:42:55 +0530

    requirments added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Medical_chatbot",
+    version="0.1.0",
+    author="Tapas Walde",
+    author_email="tapaswalde@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
